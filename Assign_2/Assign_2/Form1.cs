bfad88e93a773ead769ff5188ebf798cdaabe9da
Joe Meyer <joelawm@gmail.com>
2020-02-01 00:55:44 -0600

    Intial Commit

---
+++ Assign_2/Assign_2/Form1.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Name: Huajian Huang; zid: z1869893
+ * Partner: Joseph Meyer; zid: z1788150
+ *
+ * CSCI 473 - Assignment 2
+ * Function: The main fucntion of this program is to create a piece of software that allows the managment of properties in DeKalb but now with a GUI.
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
